7c0f1601dcc51c5d600ccac96e08229d0582ca2a
Guy Rozendorn <guy@rzn.co.il>
2014-11-13 13:29:33 +0200

    wip

--- src/infi/jira_cli/confluence_adapter.py
+++ src/infi/jira_cli/confluence_adapter.py
@@ -6,6 +6,25 @@ def get_confluence():
     from .config import Configuration
     from json_rest import JSONRestSender
     config = Configuration.from_file()
-    client = JSONRestSender("http://{0}/rest/api".format(config.confluence_fqdn))
+    client = JSONRestSender("http://{0}/rest".format(config.confluence_fqdn))
     client.set_basic_authorization(config.username, config.password)
     return client
+
+
+def get_release_notes_page_id(project_name):
+    response = get_confluence().get('/prototype/1/search/site?type=page&label=global:release-notes&label=global:{}'.format(project_name.lower()))
+    assert response['totalSize']
+    return response['result'][0]['id']
+
+
+def get_page_contents(page_id):
+    page = get_confluence().get('/api/content/{}?expand=body.view,version.number'.format(page_id))
+    print page['body']['view']['value']
+
+
+def update_page_contents(page_id, body):
+    page = get_confluence().get('/api/content/{}?expand=body.view,version.number'.format(page_id))
+    data = dict(version=dict(number=page['version']['number']+1),
+                id=page['id'], title=page['title'], type='page',
+                body=dict(representation='storage', storage=dict(value=body)))
+    get_confluence().put('api/content/%s' % page_id, data=data)
--- src/infi/jira_cli/jirelnotes.py
+++ src/infi/jira_cli/jirelnotes.py
@@ -1,17 +1,23 @@
-"""jirelease
+"""jirelnotes
 infinidat jira/confluence release-notes command-line tool
 
 Usage:
-    jirelease show {project} {version}
-    jirelease add-to-page <page-id> {project} {version}
-    jirelease publish <page-id> {project} {version}
-    jirelease fetch <page-id>
+    jirelnotes show {project} [--include-next-release]
+    jirelnotes publish {project} [--include-next-release]
+    jirelnotes notify {project} {version}
+    jirelnotes fetch
 
 Options:
     show                                 show the generated release notes
+    publish                              publish the release notes to the wiki
+    fetch                                print the release note from the wiki
+    notify                               find linked issues and put comments
     --project=PROJECT                    project key {project_default}
-    --release=RELEASE                    version string {version_default}
 """
+
+
+RELEASE_NOTES_TITLE_KEY = 'Release Notes Title'
+RELEASE_NOTES_DESCRIPTION_KEY = 'Release Notes Description'
 
 
 def _get_arguments(argv, environ):
@@ -21,7 +27,7 @@ def _get_arguments(argv, environ):
     project_default = "[default: {}]".format(environ["JISSUE_PROJECT"]) if "JISSUE_PROJECT" in environ else ""
     version_default = "[default: {}]".format(environ["JISSUE_VERSION"]) if "JISSUE_VERSION" in environ else ""
     doc_with_defaults = __doc__.format(project_default=project_default, version_default=version_default,
-                                       project="[--project=PROJECT]" if project_default else "--project=PROEJCT",
+                                       project="[--project=PROJECT]" if project_default else "--project=PROJECT",
                                        version="[--release=RELEASE]" if version_default else "--release=RELEASE")
     arguments = Munch(docopt(doc_with_defaults, argv=argv, help=True, version=__version__))
     if environ.get("JISSUE_PROJECT") and not arguments.get("--project"):
@@ -31,68 +37,102 @@ def _get_arguments(argv, environ):
     return arguments
 
 
-def render_release_notes(project_name, project_version):
+def get_issue_details(issue):
+    from .jira_adapter import get_custom_fields
+    return dict(key=issue.key,
+                title=getattr(issue.fields(), get_custom_fields()[RELEASE_NOTES_TITLE_KEY]),
+                description=getattr(issue.fields(), get_custom_fields()[RELEASE_NOTES_DESCRIPTION_KEY]))
+
+
+def is_bug(issue):
+    from .jira_adapter import issue_mappings
+    return issue_mappings['Type'](issue) == "Bug"
+
+
+def is_improvement(issue):
+    from .jira_adapter import issue_mappings
+    return issue_mappings['Type'](issue) == "Improvement"
+
+
+def is_new_feature(issue):
+    from .jira_adapter import issue_mappings
+    return issue_mappings['Type'](issue) == "New Feature"
+
+
+def get_release_notes_contents_for_specfic_version(project, version):
+    from .jira_adapter import get_custom_fields, get_jira
+    release_date = getattr(version, 'releaseDate', '')
+    base_query = "project={} AND fixVersion={!r} AND {!r} IS NOT EMPTY".format(project.key, str(version.name), RELEASE_NOTES_TITLE_KEY)
+    known_issues_query = "project={0} AND {1!r} IS NOT EMPTY AND (" \
+                         "(fixVersion IN ('known issues') AND status IN (Open, Reopened)) OR " \
+                         "(affectedVersion in ({2!r})) OR " \
+                         "(labels=known-issue AND status WAS IN (Resolved) ON {3} AND fixVersion NOT IN ({2!r})))"
+    known_issues_query = known_issues_query.format(project.key, RELEASE_NOTES_TITLE_KEY,
+                                                   str(version.name), release_date or 'now()')
+    resolved_issues = list(get_jira().search_issues(base_query))
+    known_issues = list(get_jira().search_issues(known_issues_query))
+    if resolved_issues or known_issues_query:
+        topics = [dict(name="What's new in this release", issues=[get_issue_details(issue) for issue in resolved_issues if is_new_feature(issue)]),
+                  dict(name='Improvements', issues=[get_issue_details(issue) for issue in resolved_issues if is_improvement(issue)]),
+                  dict(name='Fixed issues', issues=[get_issue_details(issue) for issue in resolved_issues if is_bug(issue)]),
+                  dict(name='Known issues', issues=[get_issue_details(issue) for issue in known_issues])]
+        return dict(name=version.name, release_date=release_date, topics=topics)
+    else:
+        return dict()
+
+
+def should_appear_in_release_notes(release):
+    return bool(release) and any(topic['issues'] for topic in release['topics'])
+
+
+def render_release_notes(project_name, project_version, include_next_release):
+    from re import match
     from jinja2 import Template
     from pkg_resources import resource_string
     from .jira_adapter import get_project, get_version, get_jira, issue_mappings, get_custom_fields
+    from .jira_adapter import get_next_release_name_in_project
 
     template = Template(resource_string('infi.jira_cli', 'release_notes.html'))
-    issue_list = list(get_jira().search_issues("project={!r} and fixVersion={!r}".format(project_name, project_version)))
-    issues = dict()
-    for issue in issue_list:
-        data = dict(key=issue.key,
-                    title=getattr(issue.fields(), get_custom_fields()["Release Notes Title"]),
-                    description=getattr(issue.fields(), get_custom_fields()['Release Notes Description']))
-        if data['title']:
-            issues.setdefault(issue_mappings['Type'](issue), list()).append(data)
-    return template.render(project=get_project(project_name), releases=[get_version(project_name, project_version)],
-                           issues=issues)
+    project = get_project(project_name)
+    real_versions = [version for version in reversed(project.versions) if
+                     match(r'[0-9\.]+', version.name) and not version.archived and
+                     version.released or (version.name == get_next_release_name_in_project(project_name) if include_next_release else False)]
+    releases = [get_release_notes_contents_for_specfic_version(project, version) for version in real_versions]
+    exposed_releases = [release for release in releases if should_appear_in_release_notes(release)]
+    return template.render(project=project, releases=exposed_releases)
 
 
-def add_to_release_notes(project_name, project_version, page_id):
-    from .confluence_adapter import get_confluence
-    release_notes = render_release_notes(project_name, project_version)
-    confluence = get_confluence()
-    page = confluence.get('content/{}?expand=body.view,version.number'.format(page_id))
-    data = dict(version=dict(number=page['version']['number']+1),
-                id=page['id'], title=page['title'], type='page',
-                body=dict(representation='storage', storage=dict(value=release_notes+page['body']['view']['value'])))
-    confluence.put('content/%s' % page_id, data=data)
+def publish_release_notes(project_name, project_version, include_next_release):
+    from .confluence_adapter import update_page_contents, get_release_notes_page_id
+    release_notes = render_release_notes(project_name, project_version, include_next_release)
+    update_page_contents(get_release_notes_page_id(project_name), release_notes)
 
 
-def publish_release_notes(project_name, project_version, page_id):
-    from .confluence_adapter import get_confluence
-    release_notes = render_release_notes(project_name, project_version)
-    confluence = get_confluence()
-    page = confluence.get('content/{}?expand=body.view,version.number'.format(page_id))
-    data = dict(version=dict(number=page['version']['number']+1),
-                id=page['id'], title=page['title'], type='page',
-                body=dict(representation='storage', storage=dict(value=release_notes)))
-    confluence.put('content/%s' % page_id, data=data)
+def show_release_notes(project_name, project_version, include_next_release):
+    print render_release_notes(project_name, project_version, include_next_release)
 
 
-def show_release_notes(project_name, project_version):
-    print render_release_notes(project_name, project_version)
+def fetch_release_notes(project_name):
+    from .confluence_adapter import get_page_contents, get_release_notes_page_id
+    print get_page_contents(get_release_notes_page_id(project_name))
 
 
-def fetch_page(page_id):
-    from .confluence_adapter import get_confluence
-    confluence = get_confluence()
-    page = confluence.get('content/{}?expand=body.view,version.number'.format(page_id))
-    print page['body']['view']['value']
+def notify_related_tickets(project_name, project_version):
+    raise NotImplementedError()
 
 
 def do_work(arguments):
     project_name = arguments['--project']
     project_version = arguments.get('--release')
     if arguments['show']:
-        show_release_notes(project_name, project_version)
-    elif arguments['add-to-page']:
-        add_to_release_notes(project_name, project_version, arguments['<page-id>'])
+        show_release_notes(project_name, project_version, arguments['--include-next-release'])
     elif arguments['publish']:
-        publish_release_notes(project_name, project_version, arguments['<page-id>'])
+        publish_release_notes(project_name, project_version, arguments['--include-next-release'])
     elif arguments['fetch']:
-        fetch_page(arguments['<page-id>'])
+        fetch_release_notes(project_name)
+    elif arguments['notify']:
+        notify_related_tickets(project_name, project_version)
+
 
 def _jiject(argv, environ):
     from sys import stderr
