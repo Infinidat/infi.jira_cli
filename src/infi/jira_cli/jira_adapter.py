cd9072d6f12dbbc803cb5c5c9be22335f6cd3a94
Guy Rozendorn <guy@rzn.co.il>
2017-08-01 15:15:48 +0300

    HOSTDEV-2745

--- src/infi/jira_cli/jira_adapter.py
+++ src/infi/jira_cli/jira_adapter.py
@@ -217,7 +217,7 @@ def _compute_value(key, value, id_lookup_method):
     return result if isinstance(result, dict) else result
 
 
-def create_issue(project_key, issue_type_name, component_name, fix_version_name, details, assignee=None, parent=None, additional_fields=None, id_lookup_method=None):
+def create_issue(project_key, issue_type_name, component_name, fix_version_name, details, priority=None, assignee=None, parent=None, additional_fields=None, id_lookup_method=None):
     jira = get_jira()
     project = jira.project(project_key)
     [issue_type] = [issue_type for issue_type in project.issueTypes
@@ -237,6 +237,8 @@ def create_issue(project_key, issue_type_name, component_name, fix_version_name,
       fields.pop('description')
     if assignee:
       fields['assignee'] = dict(name=assignee)
+    if priority:
+      fields['priority'] = dict(name=priority)
     if parent:
       fields['parent'] = dict(key=parent)
     if not versions:
