7134fb414c1dd67448e41a570c9e62db2880730d
Dor Vardi <dvardi@infinidat.com>
2024-09-29 15:28:30 +0300

    HOSTDEV-3569: fixed all the cases in the code that contain  issue.fields() to the expected syntax by python version 3.6+  issue.fields

--- src/infi/jira_cli/actions.py
+++ src/infi/jira_cli/actions.py
@@ -124,9 +124,9 @@ def show(arguments):
 
 def get(arguments):
     from .jira_adapter import get_issue, get_custom_fields
-    customfield = get_custom_fields()[arguments.get("<customfield>")]
+    customfield = get_custom_fields[arguments.get("<customfield>")]
     issue = get_issue(arguments.get("<issue>"))
-    print(getattr(issue.fields(), customfield))
+    print(getattr(issue.fields, customfield))
 
 
 def comment(arguments):
