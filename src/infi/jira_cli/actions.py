6c8e28e61455baf0dbd16d6539a42dbbee55f506
Dor Vardi <dvardi@infinidat.com>
2024-10-01 10:43:22 +0300

    HOSTDEV-3569: revert get_custom_fields changes on actions.py

--- src/infi/jira_cli/actions.py
+++ src/infi/jira_cli/actions.py
@@ -124,7 +124,7 @@ def show(arguments):
 
 def get(arguments):
     from .jira_adapter import get_issue, get_custom_fields
-    customfield = get_custom_fields[arguments.get("<customfield>")]
+    customfield = get_custom_fields()[arguments.get("<customfield>")]
     issue = get_issue(arguments.get("<issue>"))
     print(getattr(issue.fields, customfield))
 
